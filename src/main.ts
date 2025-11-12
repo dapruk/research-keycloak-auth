4e37195c0b92ac2bc1801bb8b68f377f01624563
dapruk <dapruks@gmail.com>
2025-11-12 17:05:28 +0700

    db and cookies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: true,
+    credentials: true,
+  });
+
+  app.use(cookieParser(config.get('SESSION_COOKIE_SECRET')));
+
+  const port = config.get('PORT') ?? 3000;
+  await app.listen(port);
+  console.log(`Server running on http://localhost:${port}`);
 }
 bootstrap();
